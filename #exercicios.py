a14eb4eba70171f548425b110e0d05a80f0a4cb3
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-24 02:22:25 -0300

    exercicios.py

---
+++ #exercicios.py
@@ -0,0 +1,18 @@
+#exercicios.py
+
+'''
+Aqui temos as variáveis com
+nomes de animas,quantidade de animais, peso dos animais e...
+'''
+animal1 = "gato"
+animal2 = "peixe"
+animal3 = "tartaruga"
+quantidade_de_animais = 3
+peso_animal1 = 7.8
+peso_animal2 = 3.5
+peso_animal3 = 2.5
+
+#situação de vacinação
+situacao_animal1 = True
+situação_animal2 = False
+situação_animal3 = False
