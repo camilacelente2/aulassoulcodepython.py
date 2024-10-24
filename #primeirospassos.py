4679029d8d3fa49ffa200ed3ccaf33f051d497ec
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-24 01:49:04 -0300

    primeirospassos.py

---
+++ #primeirospassos.py
@@ -0,0 +1,20 @@
+#primeirospassos
+
+#Aula 01 - Introdução a Linguagem Phyton (Comentários).
+#Esta linha imprime um texto.
+print("Olá mundo!")
+
+'''
+Este é um comentário
+longo do
+Phyton.
+'''
+
+"""
+Este é um comentário
+longo do
+Phyton.
+"""
+
+#print("Olá mundo!")
+
