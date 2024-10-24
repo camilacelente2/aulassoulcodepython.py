b617d03baa575641b2a9f66e25d39425784609d4
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-24 02:15:01 -0300

    tipos de dados.py

---
+++ #tipos de dados.py
@@ -0,0 +1,14 @@
+#tipos de dados.py
+
+idade = 32
+peso = 65.4
+nome = "camila"
+peso2 = "65.4"
+situação = True
+situação = False
+type (idade)
+type (peso)
+#string
+type (nome)
+#booleana
+type (situação)
