ea127639241f3f6f2ddac07a6958c476eb1de1d2
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-24 01:59:35 -0300

    indentacao

--- #indentacao.py
+++ #indentacao.py
@@ -2,10 +2,10 @@
 
 X = 0
 Y = 0
-if x ==0:
+if "x" ==0:
   print("X é igual a 10.")
   print("X é igual a 10.")
-  if y == 0:
+  if "y" == 0:
     print("X é igual a 10.")
 
 print("X é igual a 10.")
