d44b7ec714456f9f85d7545b31532e98c645e771
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 14:58:22 -0300

    entradadedados.py

---
+++ entradadedados.py
@@ -0,0 +1,9 @@
+#entrada de dados.
+
+nome = input("Escreva seu nome: ")#String
+idade = int(input("Escreva sua idade: "))#Inteiro
+peso = float(input("Escreva seu peso: "))#float
+
+print(nome)
+print(idade)
+print(peso)
