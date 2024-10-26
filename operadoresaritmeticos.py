11be2b1a069b183882a7c0843cc9f67e909827d2
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 15:21:35 -0300

    operadoresaritmeticos.py

---
+++ operadoresaritmeticos.py
@@ -0,0 +1,11 @@
+#operadoresaritmeticos.py
+
+# +, -, /, *, //, %
+a = 4
+b = 4
+print(a + b)#adição
+print(5 - 2)#subtração
+print(2 * 5)#multiplicação
+print(10 / 6)#divisão
+print(10 // 6)#divisão exata
+print(10 % 3)
