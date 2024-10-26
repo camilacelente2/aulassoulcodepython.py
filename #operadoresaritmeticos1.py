963c2137d6f9e10d7f0db7896229509f48113f7e
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 15:24:07 -0300

    operadoresaritimeticos1.py

---
+++ #operadoresaritmeticos1.py
@@ -0,0 +1,6 @@
+#operadoresaritmeticos1
+
+nome = input("Digite seu nome: ")#String
+altura = float(input("Digite sua altura: "))#float
+cpf = int(input("Digite seu cpf: "))#inteiro
+print("O nome digitado é: ", nome, ". A altura digitada é: ", altura, ". O CPF digitado é: ", cpf,".")
