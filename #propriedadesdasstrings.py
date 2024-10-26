3da6ee86bc7429fd1a798b8b890c7fb25627e320
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 16:03:35 -0300

    propriedadesdasstrings.py

---
+++ #propriedadesdasstrings.py
@@ -0,0 +1,14 @@
+#propriedadesdasstrings.py
+
+l = 'lista de letras'
+data = '15/09/1991'
+#tamanho da string
+tamanho = len (l)
+print(tamanho)
+#split da string
+lista = l.split(" ")
+print(lista)
+lista2 = data.split("/")
+print(lista2)
+#substituição
+print(l.replace(" de ", ""))
