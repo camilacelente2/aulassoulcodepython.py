dfd0c1cf30fbeda86b3b333f0d397deebcbadd95
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 15:35:55 -0300

    correcaodeexercicios.py

---
+++ #correcaodeexercicios.py
@@ -0,0 +1,9 @@
+#correcaodeexercicios.py
+
+numero = 15
+if numero == 15:
+  print("Este numero é igual a 15!")
+elif mnumero < 15:
+  print("Este numero é menor que 15!")
+elif numero > 15:
+  print("Este numero é maior que 15!")
