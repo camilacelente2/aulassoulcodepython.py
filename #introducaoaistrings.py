2747cab0ab7321bf4c3a894d38af2e70cbeb547d
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 16:01:00 -0300

    introducaoaistrings.py

---
+++ #introducaoaistrings.py
@@ -0,0 +1,17 @@
+#introducaoaistrings.py
+
+nome = "Camila Celente Eggert"
+type(nome)
+nome2 = 'Camila Celente Eggert'
+nome3 = '''
+Esta é uma string longa
+contendo informações sobre Python
+'''
+nome4 = """
+Esta é outra string longa
+contendo informações sobre Python
+"""
+print(nome)
+print(nome2)
+print(nome3)
+print(nome4)
