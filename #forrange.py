85c12bef5e1499126e2d966847deaa7324de0263
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 15:48:57 -0300

    forrange.py

---
+++ #forrange.py
@@ -0,0 +1,14 @@
+#forrange.py
+
+for i in range(10):
+  print("olá mundo")
+  
+  #for range continuação
+  
+  for j in range(9, -1, -1):
+    print(j)
+    
+    #for range continuação----
+    
+for i in range(0, 18, 3):
+  print(i)
