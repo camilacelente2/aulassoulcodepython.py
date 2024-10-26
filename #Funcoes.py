e8e2d92fd6de1e41015b1e0f0fe06c91a7671269
camilacelente2 <camilacelente@yahoo.com.br>
2024-10-26 16:36:03 -0300

    funcoes.py

---
+++ #Funcoes.py
@@ -0,0 +1,25 @@
+#Funcoes.py
+
+#Introdução a Funções
+def funcao():
+  print("Eu sou uma função!")
+funcao()
+
+#Parametro de funções
+def soma(x, y):
+  total = x + y
+  print(total)
+
+soma(2,4)
+
+#Retorno
+def show():
+  return 15
+
+print(show())
+
+#Retorno(1)
+def mult(a, b):
+  return a * b
+
+print(mult(2,5))
